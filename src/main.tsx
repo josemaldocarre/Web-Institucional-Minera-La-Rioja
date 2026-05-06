bea938f204a70dffdbb0adadf98503973fd0b96a
Aldana Baurer <aldanabaurer@gmail.com>
2026-05-05 23:01:12 -0300

    feat(styles): add design system with SASS variables and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n/config'
-import './styles/global.scss'
+import './styles/globals.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
